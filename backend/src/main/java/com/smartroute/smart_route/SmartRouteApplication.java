0d5be4cbb2ebb98d13e538a468c1ef3b8c68462f
Lukas Schatzer <e12223229@student.tuwien.ac.at>
2025-10-30 02:25:21 +0100

    Initial commit

---
+++ backend/src/main/java/com/smartroute/smart_route/SmartRouteApplication.java
@@ -0,0 +1,13 @@
+package com.smartroute.smart_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRouteApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SmartRouteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartroute/smart_route/SmartRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartroute.smart_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
