2a71a5a4bf2e940bb291f2cd12e2b91e0ab45de9
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2025-11-10 14:30:55 +0100

    #10 Change project structure so that it separates components, services, dtos, etc

--- frontend/src/app/components/trainingPlan/trainingPlan.page.ts
+++ frontend/src/app/components/trainingPlan/trainingPlan.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trainingplan',
-  templateUrl: 'trainingplan.page.html',
-  styleUrls: ['trainingplan.page.scss'],
+  templateUrl: 'trainingPlan.page.html',
+  styleUrls: ['trainingPlan.page.scss'],
   standalone: false,
 })
 export class TrainingPlanPage {
