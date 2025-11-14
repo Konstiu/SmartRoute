5b431f08cc5c306312e83b3a908259d4c71a56ad
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2025-11-14 19:59:29 +0100

    #FIX Change backendUri in frontend from 8081 to 8080

--- frontend/src/global/globals.ts
+++ frontend/src/global/globals.ts
@@ -2,5 +2,5 @@ import {Injectable} from "@angular/core";
 
 @Injectable({providedIn: "root"})
 export class Globals {
-  readonly backendUri: string = 'http://localhost:8081/api/v1';
+  readonly backendUri: string = 'http://localhost:8080/api/v1';
 }
