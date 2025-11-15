459ab0f695737c5fd5fb8c6b7d04733151ed1ade
Rafael Milchram <rafael.milchram@gmail.com>
2025-11-15 01:07:02 +0100

    feat: add strava athlete detail import logic (#16)

--- backend/src/main/java/com/smartroute/smartroute1/repository/StravaActivityRepository.java
+++ backend/src/main/java/com/smartroute/smartroute1/repository/StravaActivityRepository.java
@@ -1,7 +1,11 @@
 package com.smartroute.smartroute1.repository;
 
+import com.smartroute.smartroute1.entity.StravaAccount;
 import com.smartroute.smartroute1.entity.StravaActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StravaActivityRepository extends JpaRepository<StravaActivity, Long> {
+    List<StravaActivity> findByStravaAccount(StravaAccount account);
 }
