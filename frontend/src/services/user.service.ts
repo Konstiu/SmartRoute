2a36e5322a89a8ad06d278248dd3e292182f8b85
Johannes Pritzl <e12216466@student.tuwien.ac.at>
2025-11-17 17:54:49 +0100

    Implemented component for changing the users personal data like height and birthday. The request still needs the token to be added.

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,17 +1,21 @@
 import { Globals } from "../global/globals";
-import {HttpClient} from "@angular/common/http";
-import {CreateUserDto, UserDto} from "../app/dtos/user";
-import {Observable} from "rxjs";
-import {inject, Injectable} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { CreateUserDto, PersonalDataDto, UserDetailDto, UserDto } from "../app/dtos/user";
+import { Observable } from "rxjs";
+import { inject, Injectable } from "@angular/core";
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: "root" })
 export class UserService {
   private httpClient = inject(HttpClient);
   private globals = inject(Globals);
   private userUri: string = this.globals.backendUri + '/user';
 
 
-    createUser(toCreate: CreateUserDto): Observable<UserDto> {
-      return this.httpClient.post<UserDto>(this.userUri, toCreate, {responseType:'json'});
-    }
+  createUser(toCreate: CreateUserDto): Observable<UserDto> {
+    return this.httpClient.post<UserDto>(this.userUri, toCreate, { responseType: 'json' });
+  }
+
+  updatePersonalData(updateInformation: PersonalDataDto): Observable<UserDetailDto> {
+    return this.httpClient.put<UserDetailDto>(this.userUri + "/personal-data", updateInformation, { responseType: 'json' });
+  }
 }
