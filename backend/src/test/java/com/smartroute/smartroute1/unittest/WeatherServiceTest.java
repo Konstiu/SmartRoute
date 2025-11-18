c8e86dbb8c47fb31dca49c6540d04969c3b7dc16
Frederic Lechner <e12514056@student.tuwien.ac.at>
2025-11-18 12:07:57 +0100

    test(weather): changed asserts and the method name and description to be more clear

--- backend/src/test/java/com/smartroute/smartroute1/unittest/WeatherServiceTest.java
+++ backend/src/test/java/com/smartroute/smartroute1/unittest/WeatherServiceTest.java
@@ -1,6 +1,7 @@
 package com.smartroute.smartroute1.unittest;
 
 import com.smartroute.smartroute1.entity.WeatherResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,26 +9,40 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class WeatherServiceTest {
-
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @DisplayName("GET /api/weather returns 200 OK and valid hourly weather data")
     @Test
-    void testWeatherEndpointReturnsData() {
+    void testWeatherEndpointReturnsData_200() {
         String url = "/api/weather?lat=48.210033&lon=16.363449";
         ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(url, WeatherResponse.class);
 
         final WeatherResponse body = response.getBody();
 
-        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-        assertThat(body).isNotNull();
-        assertThat(body.getTime()).isNotEmpty();
-        assertThat(body.getTemperature2m()).isNotEmpty();
-        assertThat(body.getWindSpeed10m()).isNotEmpty();
-        assertThat(body.getPrecipitation()).isNotEmpty();
+        assertAll(
+                () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                () -> assertNotNull(body),
+                () -> {
+                    assertNotNull(body);
+                    assertFalse(body.getTime().isEmpty());
+                },
+                () -> {
+                    assertNotNull(body);
+                    assertFalse(body.getTemperature2m().isEmpty());
+                },
+                () -> {
+                    assertNotNull(body);
+                    assertFalse(body.getWindSpeed10m().isEmpty());
+                },
+                () -> {
+                    assertNotNull(body);
+                    assertFalse(body.getPrecipitation().isEmpty());
+                }
+        );
     }
 }
