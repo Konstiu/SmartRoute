2e7ba9036ed2a92929141636ce15b5d6ce94b803
Frederic Lechner <e12514056@student.tuwien.ac.at>
2025-11-18 12:15:57 +0100

    feat(weather): added basic excpetion handler for (weather) api

--- backend/src/main/java/com/smartroute/smartroute1/service/impl/WeatherServiceImpl.java
+++ backend/src/main/java/com/smartroute/smartroute1/service/impl/WeatherServiceImpl.java
@@ -3,18 +3,22 @@ package com.smartroute.smartroute1.service.impl;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smartroute.smartroute1.entity.WeatherResponse;
+import com.smartroute.smartroute1.exception.ApiException;
 import com.smartroute.smartroute1.service.WeatherService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.ArrayList;
 
 @Service
 public class WeatherServiceImpl implements WeatherService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final RestTemplate restTemplate = new RestTemplate();
     private final ObjectMapper mapper = new ObjectMapper();
 
@@ -25,6 +29,8 @@ public class WeatherServiceImpl implements WeatherService {
                 .queryParam("longitude", longitude)
                 .queryParam("hourly", "temperature_2m,precipitation,wind_speed_10m")
                 .toUriString();
+
+        LOGGER.trace("Calling Open-Meteo API with URL: {}", url);
 
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
@@ -53,7 +59,8 @@ public class WeatherServiceImpl implements WeatherService {
 
             return new WeatherResponse(time, temperature2m, windSpeed10m, precipitation);
         } catch (Exception e) {
-            throw new RuntimeException("Error fetching or parsing hourly weather data", e);
+            LOGGER.warn("Error fetching or parsing hourly weather data", e);
+            throw new ApiException("Failed to retrieve hourly weather data", e);
         }
     }
 }
