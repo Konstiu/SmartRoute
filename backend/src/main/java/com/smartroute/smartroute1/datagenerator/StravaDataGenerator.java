22313fba1152b992ceb6d491232d609e0871cb6e
Konsti <konstantin.unterweger@gmail.com>
2025-11-22 20:53:26 +0100

    chore: cleanup datagen implementation and add datagen for injuries
    - remove manual ID assignment in generators
    - fix sequencing with @DependsOn

--- backend/src/main/java/com/smartroute/smartroute1/datagenerator/StravaDataGenerator.java
+++ backend/src/main/java/com/smartroute/smartroute1/datagenerator/StravaDataGenerator.java
@@ -10,8 +10,8 @@ import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
@@ -19,7 +19,7 @@ import java.time.Instant;
 import java.util.List;
 
 @Profile("generateData")
-@Order(2)
+@DependsOn("userDataGenerator")
 @Component
 @AllArgsConstructor
 public class StravaDataGenerator {
@@ -37,26 +37,24 @@ public class StravaDataGenerator {
             LOGGER.info("Accounts already generated");
         } else {
             LOGGER.info("generating {} Strava account entries", userList.size());
-            long id = 0;
+
             for (ApplicationUser user : userList) {
                 StravaAccount acc = new StravaAccount();
                 acc.setUser(user);
                 acc.setScopes("read,activity:read_all,profile:read_all");
-                acc.setId(id);
                 acc.setConnectedAt(Instant.now());
-                acc.setAthleteId(id);
-                acc.setAccessToken("DummyAccessToken" + id);
-                acc.setRefreshToken("DummyRefreshToken" + id);
+                acc.setAthleteId(user.getId());
+                acc.setAccessToken("DummyAccessToken" + user.getId());
+                acc.setRefreshToken("DummyRefreshToken" + user.getId());
                 acc.setExpiresAt(Instant.now().plusSeconds(300));
                 stravaAccountRepository.save(acc);
-                LOGGER.info("saving account for user {} ", user.getId());
-
-
+                LOGGER.debug("saving account for user {} ", user.getId());
             }
         }
 
         generateActivities();
     }
+
 
     private void generateActivities() {
         List<StravaAccount> stravaAccountList = stravaAccountRepository.findAll();
@@ -66,7 +64,7 @@ public class StravaDataGenerator {
         } else {
             long id = 0;
             for (StravaAccount acc : stravaAccountList) {
-                LOGGER.info("generating {} Strava activities for user {}", NUMBER_OF_ACTIVITIES_PER_USER, acc.getUser().getEmail());
+                LOGGER.debug("generating {} Strava activities for user {}", NUMBER_OF_ACTIVITIES_PER_USER, acc.getUser().getEmail());
 
                 for (int i = 0; i < NUMBER_OF_ACTIVITIES_PER_USER; i++) {
                     StravaActivity sa = new StravaActivity();
