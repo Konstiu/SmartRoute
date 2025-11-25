8931f063571193577460f69355be2f2fed736d99
Konsti <konstantin.unterweger@gmail.com>
2025-11-25 21:03:20 +0100

    fix: add findBackendUrl finder

--- frontend/src/global/globals.ts
+++ frontend/src/global/globals.ts
@@ -2,5 +2,14 @@ import {Injectable} from "@angular/core";
 
 @Injectable({providedIn: "root"})
 export class Globals {
-  readonly backendUri: string = 'http://localhost:8080/api/v1';
+  readonly backendUri: string = this.findBackendUrl();
+
+  private findBackendUrl(): string {
+    if (window.location.port === '8100') { // local `ionic`, backend at localhost:8080
+      return 'http://localhost:8080/api/v1';
+    } else {
+      // assume deployed somewhere and backend is available at same host/port as frontend
+      return window.location.protocol + '//' + window.location.host + '/api/v1';
+    }
+  }
 }
