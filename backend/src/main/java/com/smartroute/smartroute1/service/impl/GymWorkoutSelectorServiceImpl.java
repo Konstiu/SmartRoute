383eb92bd30b1b9b48f627d7ae0b3d190ebcc4f6
Sebastian Schnitzer <e11825345@student.tuwien.ac.at>
2025-12-01 18:20:28 +0100

    #40 Remove checkstyle violation in GymWorkoutSelectorServiceImpl

--- backend/src/main/java/com/smartroute/smartroute1/service/impl/GymWorkoutSelectorServiceImpl.java
+++ backend/src/main/java/com/smartroute/smartroute1/service/impl/GymWorkoutSelectorServiceImpl.java
@@ -9,7 +9,16 @@ import com.smartroute.smartroute1.service.GymWorkoutSelectorService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 @Service
 public class GymWorkoutSelectorServiceImpl implements GymWorkoutSelectorService {
