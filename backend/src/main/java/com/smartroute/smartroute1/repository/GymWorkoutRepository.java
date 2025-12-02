c7edc990eeb8b02a0cf4fd4bcf6b51af958338fb
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2025-12-02 19:08:07 +0100

    #65 Add method in GymWorkoutSelectorService so that it saves gymworkouts to a certain user

---
+++ backend/src/main/java/com/smartroute/smartroute1/repository/GymWorkoutRepository.java
@@ -0,0 +1,12 @@
+package com.smartroute.smartroute1.repository;
+
+import com.smartroute.smartroute1.entity.ApplicationUser;
+import com.smartroute.smartroute1.entity.GymWorkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GymWorkoutRepository extends JpaRepository<GymWorkout, Long> {
+
+    List<GymWorkout> findAllByUser(ApplicationUser user);
+}
