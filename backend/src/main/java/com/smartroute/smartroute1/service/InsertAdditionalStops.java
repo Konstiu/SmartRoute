73156820e9f2a6a60a516b9b56fc2c18d3f3e8da
Frederic Lechner <e12514056@student.tuwien.ac.at>
2025-12-05 22:14:04 +0100

    test(addStops): add test basics.

--- backend/src/main/java/com/smartroute/smartroute1/service/InsertAdditionalStops.java
+++ backend/src/main/java/com/smartroute/smartroute1/service/InsertAdditionalStops.java
@@ -3,29 +3,48 @@ package com.smartroute.smartroute1.service;
 import com.smartroute.smartroute1.exception.ValidationException;
 import com.smartroute.smartroute1.util.Coordinate;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface InsertAdditionalStops {
 
     /**
-     *  Request a route from start -> via -> end and return polyline as list of Coordinates.
+     * Request a route from start -> via -> end and return polyline as list of Coordinates.
      *
      * @param start the coordinate where the new segment should begin
-     * @param via the user-defined coordinate that the route must visit
-     * @param end the coordinate where the new segment should end
+     * @param via   the user-defined coordinate that the route must visit
+     * @param end   the coordinate where the new segment should end
      * @return an ordered list of {@link Coordinate} representing the computed route from start through via to end
      * @throws IllegalStateException if the routing provider returns an invalid or empty response
      */
-    public List<Coordinate> routeThroughPoint(Coordinate start, Coordinate via, Coordinate end) throws ValidationException;
+    List<Coordinate> routeThroughPoint(Coordinate start, Coordinate via, Coordinate end) throws ValidationException;
 
 
     /**
      * Computes optimal endpoints on original route, generates new route segments and stitches them together.
      *
      * @param originalRoute the full list of coordinates representing the existing route; must contain at least two points
-     * @param newPoints list of {@link Coordinate} that should be added to the rout
+     * @param newPoints     list of {@link Coordinate} that should be added to the rout
      * @return a new list of {@link Coordinate} representing the updated route including the waypoint
      * @throws IllegalArgumentException if the input route is null, too short, or contains invalid data
      */
-    public List<Coordinate> addWaypoints(List<Coordinate> originalRoute, List<Coordinate> newPoints) throws ValidationException;
+    List<Coordinate> addWaypoints(List<Coordinate> originalRoute, List<Coordinate> newPoints) throws ValidationException;
+
+    /**
+     * Extracts a list of coordinates from a given .gpx file.
+     *
+     * @param pathname name of the path where the gpx file should be read from
+     * @return list of {@link Coordinate}, the extracted polyline
+     * @throws IOException if the file cannot be opened, read, or parsed successfully
+     */
+    List<Coordinate> gpxToPolyline(String pathname) throws IOException;
+
+    /**
+     * Build a .gpx file from a given list of coordinates.
+     *
+     * @param coordinates list of {@link Coordinate} that should be included in the .gpx file
+     * @param pathname    name of the path where the gpx file should be saved in
+     * @throws IOException if the file cannot be created, written to, or closed properly
+     */
+    void createGpx(List<Coordinate> coordinates, String pathname) throws IOException;
 }
