d5f475501831f21d1a1a41b9c1fef440400b0507
Konsti <konstantin.unterweger@gmail.com>
2025-12-21 21:31:22 +0100

    feat: App can be installed via PWA - tested on IOS and Linux fedora via Chrome. - On Android was not tested. (#93)

--- frontend/src/app/components/app.module.ts
+++ frontend/src/app/components/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {IonicModule} from '@ionic/angular';
@@ -7,10 +7,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {authInterceptor} from '../../interceptors/auth-interceptor';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     provideHttpClient(
       withInterceptors([authInterceptor])
