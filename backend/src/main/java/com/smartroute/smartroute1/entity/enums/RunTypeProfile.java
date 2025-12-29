a614e197f0cf4653bd217ac2c058eaac51478f7c
Sebastian Schnitzer <e11825345@student.tuwien.ac.at>
2025-12-29 16:50:06 +0100

    #94 Change RunTypeProfile to also display usual zone percentages

--- backend/src/main/java/com/smartroute/smartroute1/entity/enums/RunTypeProfile.java
+++ backend/src/main/java/com/smartroute/smartroute1/entity/enums/RunTypeProfile.java
@@ -4,5 +4,10 @@ public record RunTypeProfile(double paceMultiplier,
                              double distanceMultiplier,
                              double elevationMultiplier,
                              int zone3to5Bias,
-                             int paceSpikes) {
+                             int paceSpikes,
+                             double zone5p,
+                             double zone4p,
+                             double zone3p,
+                             double zone2p,
+                             double zone1p) {
 }
