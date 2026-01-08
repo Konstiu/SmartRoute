23350fb7ff4c1867cd52a908f4df07a4edea6769
Konsti <konstantin.unterweger@gmail.com>
2026-01-08 13:37:48 +0100

    wip: qrcode based key transfer

---
+++ backend/src/main/java/com/smartroute/smartroute1/endpoint/dto/keysync/DownloadKeysResponseDto.java
@@ -0,0 +1,12 @@
+package com.smartroute.smartroute1.endpoint.dto.keysync;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DownloadKeysResponseDto {
+    private String encryptedData;
+}
