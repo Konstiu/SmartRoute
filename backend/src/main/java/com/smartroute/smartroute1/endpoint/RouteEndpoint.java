eae8788c68cbeb0ded89c50c0df08333b6178d67
Sebastian Schnitzer <e11825345@student.tuwien.ac.at>
2026-01-10 15:34:31 +0100

    #50 Fix WorkoutTypeSelectorServiceTest so it works with new RouteService

--- backend/src/main/java/com/smartroute/smartroute1/endpoint/RouteEndpoint.java
+++ backend/src/main/java/com/smartroute/smartroute1/endpoint/RouteEndpoint.java
@@ -9,6 +9,7 @@ import com.smartroute.smartroute1.entity.ApplicationUser;
 import com.smartroute.smartroute1.service.OpenRouteServiceService;
 import com.smartroute.smartroute1.service.RouteService;
 import com.smartroute.smartroute1.service.impl.CustomUserDetailService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.security.PermitAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +18,15 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
@@ -25,6 +34,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/route")
+@Tag(name = "Route Endpoint")
 public class RouteEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
