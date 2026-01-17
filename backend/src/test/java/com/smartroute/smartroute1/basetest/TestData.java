7339d28d89166999d0c1fdb64349fdb81d370a16
Konsti <konstantin.unterweger@gmail.com>
2026-01-17 11:14:56 +0100

    feat: add testcases + javaDoc (#45)

--- backend/src/test/java/com/smartroute/smartroute1/basetest/TestData.java
+++ backend/src/test/java/com/smartroute/smartroute1/basetest/TestData.java
@@ -2,17 +2,19 @@ package com.smartroute.smartroute1.basetest;
 
 public interface TestData {
 
-	String BASE_URI = "/api/v1";
+    String BASE_URI = "/api/v1";
 
-	String LOGIN_BASE_URI = BASE_URI + "/authentication";
+    String LOGIN_BASE_URI = BASE_URI + "/authentication";
 
-	String USER_BASE_URI = BASE_URI + "/user";
+    String USER_BASE_URI = BASE_URI + "/user";
 
-	String USER_INJURY_BASE_URI = USER_BASE_URI + "/injuries";
+    String USER_INJURY_BASE_URI = USER_BASE_URI + "/injuries";
 
-	String DEFAULT_USER_EMAIL = "email0@smartroute.com";
+    String DEFAULT_USER_EMAIL = "email0@smartroute.com";
 
-	String ORIGIN = "http://localhost:8100";
+    String ORIGIN = "http://localhost:8100";
+
+    String SAMPLE_POLYLINE = "szdeHix{bB@Oe@OKCEd@E\\AFUtB?DCTGC?V?@E^ITOTCDjB~DBADHFL?DpAbDBHP`@DHFJFNGHLVbAvBDHDJZp@Xp@p@|AL^MLRj@IFGHQHOLZxA@Tb@vDCTIBPvAKBKDJx@Fr@PpAV~BCBkBlBoArAi@n@_A}DIVy@|AYp@CBGHABMNCDMRSTGHCDILIHABILSXu@bAMRQR_@gBO{@QGoB~@CHa@REEyBdACHQHOHECSHq@ZqAiLI_@OCOCa@LgFdBEEQc@eAwC?GKYQa@IOEIKUEIAECDEFSXkB{DKU}AaDQKG?HQH]FOKe@G[ESPWEOzDyFg@gAUe@CGLQNSBEBEXa@T]@AHMKSoAaCIQRWz@bBZn@QVJRx@bBFNhD}EHMN@CG@Kp@cA`AcHNSD?Hi@EOGE`AkHD_@F]BS@EB@BEFBZP\\NH?B@JFAFxAj@xB~@HI?Of@TJDRHL^@FJJJJLFRBv@?Rg@j@s@Xa@DETYLOHYDB@HBHj@PD@D@D@JBd@NAN";
 
 
 }
