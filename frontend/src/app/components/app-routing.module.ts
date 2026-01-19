d6a2b4c62358f5bf22c00d6daae6f00be002e1d6
Konsti <konstantin.unterweger@gmail.com>
2026-01-19 12:01:49 +0100

    feat: share route to friends

--- frontend/src/app/components/app-routing.module.ts
+++ frontend/src/app/components/app-routing.module.ts
@@ -76,6 +76,10 @@ const routes: Routes = [
   {
     path: '**',
     loadComponent: () => import('./not-found/not-found.page').then(m => m.NotFoundPage),
+  },
+  {
+    path: 'share-route-friends',
+    loadChildren: () => import('./share-route-friends/share-route-friends.module').then( m => m.ShareRouteFriendsPageModule)
   }
 
 ];
