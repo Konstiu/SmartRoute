defcca6e5e787bb929d113a538e529cfa849e07d
Sebastian Schnitzer <e11825345@student.tuwien.ac.at>
2026-01-23 19:58:37 +0100

    #121 Save user statistics in backend to make it faster

---
+++ backend/src/main/java/com/smartroute/smartroute1/repository/statistics/AtlRepository.java
@@ -0,0 +1,14 @@
+package com.smartroute.smartroute1.repository.statistics;
+
+
+import com.smartroute.smartroute1.entity.ApplicationUser;
+import com.smartroute.smartroute1.entity.Atl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.List;
+
+public interface AtlRepository extends JpaRepository<Atl, Double> {
+
+    List<Atl> getAtlByUserAndDateBetween(ApplicationUser user, Instant from, Instant to);
+}
